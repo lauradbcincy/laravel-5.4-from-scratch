29b0695b2b90b84d9e9369579aa18133622329e8
Laura B <phoenix@phoenixrc.com>
2018-09-13 12:44:41 -0400

    Episode 11: Form Request Data and CSRF

--- code/blog/routes/web.php
+++ code/blog/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/create', 'PostsController@create');
+//Route::get('/posts/{post}', 'PostsController@show');
 
