fd8e9cc4c0f00a7bf3ed3d7347152f76fa699c70
Laura B <phoenix@phoenixrc.com>
2018-09-14 13:55:54 -0400

    Episode 15: Eloquent Relationships and Comments

---
+++ code/blog/app/Comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use \App\Post;
+
+class Comment extends Model
+{
+  public function post()
+  {
+      return $this->belongsTo(Post::class);
+  }
+}
--- code/blog/resources/views/posts/show.blade.php
+++ code/blog/resources/views/posts/show.blade.php
@@ -1,13 +1,35 @@
-
 @extends('layouts.master')
 
 @section('content')
 
-  <h1>{{ $post->title }}</h1>
+<h1>{{ $post->title }}</h1>
 
-  <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
+<p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
 
-  {{ $post->body }}
+{{ $post->body }}
 
+<hr />
+
+<div class="comments">
+
+    <ul class="list-group">
+
+        @foreach  ($post->comments as $comment)
+
+        <li class="list-group-item">
+
+            <strong>
+              {{ $comment->created_at->diffForHumans()}}: &nbsp;
+            </strong>
+
+            {{ $comment->body }}
+
+        </li>
+
+        @endforeach
+
+    </ul>
+
+</div>
 
 @endsection
