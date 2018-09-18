a5e8317ed4e3a198d0fc84ac5389bd4ad69d66b7
Laura B <phoenix@phoenixrc.com>
2018-09-18 15:50:12 -0400

    add back a bit he deleted but we want for the next episode

--- code/blog/routes/web.php
+++ code/blog/routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+App::singleton('App\Billing\Stripe', function () {
+  return new \App\Billing\Stripe(config('services.stripe.secret'));
+});
+
+
+// resolve() and also app() are aliases for App::make
+dd(resolve('App\Billing\Stripe'));
+
+
 Route::get('/', 'PostsController@index')->name('home');
 
 Route::redirect('/home', '/');
